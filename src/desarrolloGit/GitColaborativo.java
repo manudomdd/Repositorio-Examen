31978bae5b5203ec9f094ec9a9409e3fa359a32e
Manu <dominguez.dieguez.manuel.jesus@iescamas.es>
2024-12-17 16:24:38 +0100

    cambios examen

--- src/desarrolloGit/GitColaborativo.java
+++ src/desarrolloGit/GitColaborativo.java
@@ -1,4 +1,4 @@
-package desarrolloGit;
+package desarrollogit;
 import java.util.Scanner;
 
 public class GitColaborativo {
@@ -6,7 +6,7 @@ public class GitColaborativo {
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
 		
-		double descvip = 0.95;
+		double final descvip = 0.95;
 		
 		//cliente introduce los km
 		System.out.println("Introduzca el numero de km que ha conducido: ");
@@ -21,26 +21,24 @@ public class GitColaborativo {
 		double p2 = (km - 1000) * 0.15 + (300 + 200);
 		
 		//Cálculo facturación y visualización del total
-		if((km < 300) && (vip == 1))
-		System.out.println("Total:" + 200 * descvip );
+		if((km < 300) && (vip == 1)) {
+		System.out.println("Total:" + 200 * descvip);
 		
-		else if((km < 300) && (vip == 2))
+		} else if((km < 300) && (vip == 2)) {
 		System.out.println("Total: " + 200);
 		
-		else if((km >=300) && (km <= 1000) && (vip == 2 ))
+		} else if((km >=300) && (km <= 1000) && (vip == 2)) {
 		System.out.println("Total no vip: " + (p1 * 1.21));
 		
-		else if((km >= 300) && (km <= 1000) && (vip == 1))
+		} else if((km >= 300) && (km <= 1000) && (vip == 1)) {
 		System.out.println("Total:" + (p1 * 0.95) * 1.21 );
 		
-		else if((km > 1000) && (vip == 2))
+		} else if((km > 1000) && (vip == 2)) {
 		System.out.println("Total no vip" + (p2 * 1.21));
 		
-		else if((km > 1000) && (vip == 1))
+		} else if((km > 1000) && (vip == 1)) {
 		System.out.println("Total vip: " + (p2 * 0.95 ) * 1.21 );
-			
+		}		
 		sc.close();
-	
 	}
-
 }
